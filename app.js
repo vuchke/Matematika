a004680a3bf759ab1c12e51ca0593c72de47f90c
Vuchke <vuchke@gmail.com>
2020-07-31 19:06:21 +0200

    new section + animations

--- app.js
+++ app.js
@@ -40,10 +40,42 @@ const tilt = () => {
   });
 }
 
+// Scroll Reveal 
+const Scroll = () => {
+  window.sr = ScrollReveal();
+
+  sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+  })
+  sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  })
+
+  sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  })
+  sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+  })
+}
+
 // here we call all functions
 const app = () => {
   navSlide();
   tilt();
+  Scroll()
 };
 // we invoke all the functions
 app();
